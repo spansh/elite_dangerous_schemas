36084e544d199238bc12dfcfe6cd0213ae590ab1
Gareth Harper <git@spansh.co.uk>
2026-03-10 15:47:12 +0000

    added system check

--- test/spec.js
+++ test/spec.js
@@ -6,10 +6,24 @@ const matchApiSchema = require('api-contract-validator').chaiPlugin;
 // API definitions path
 const apiDefinitionsPath = path.join(__dirname, '../api.openapi.json'); 
 
+
 // add as chai plugin
 use(matchApiSchema({ apiDefinitionsPath }));
 
-it('GET /api/dump/10477373803', async () => {
-    const response = await axios.get('https://spansh.co.uk/api/dump/10477373803');
-    expect(response).to.have.status(200).and.to.matchApiSchema();
-})
+const requests = [
+    {
+        method: 'get',
+        path: '/api/dump/10477373803'
+    },
+    {
+        method: 'get',
+        path: '/api/system/10477373803'
+    }
+];
+
+for (const request of requests) {
+    it(`${request.method.toUpperCase()} ${request.path}`, async () => {
+        const response = await axios[request.method](`https://spansh.co.uk${request.path}`);
+        expect(response).to.have.status(200).and.to.matchApiSchema();
+    })
+}
