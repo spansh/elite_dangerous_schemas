a04cadde08c9bef573b59a94384993f234a88f45
Gareth Harper <git@spansh.co.uk>
2026-03-12 14:31:15 +0000

    added system downloads to check

--- test/downloads.js
+++ test/downloads.js
@@ -14,12 +14,16 @@ const requests = [
     {
         method: 'get',
         path: '/galaxy_sol_shinrarta_colonia.json'
+    },
+    {
+        method: 'get',
+        path: '/systems_1day.json'
     }
 ];
 
 for (const request of requests) {
     it(`${request.method.toUpperCase()} ${request.path} passes OpenAPI check`, async () => {
         const response = await axios[request.method](`https://downloads.spansh.co.uk${request.path}`);
-        expect(response).to.have.status(200).and.to.matchApiSchema();
+        expect(response).to.be.successful().and.to.matchApiSchema();
     }).timeout(60000);
 }
